82f0fab07d1676d639215f2fe42fc079868a4b28
Дмитрий <proton0993ak@gmail.com>
2018-05-29 17:17:06 +0300

    290518

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -11,3 +11,10 @@ $(function(){
     });
 
 });
+
+
+
+$(".wrapper .tab").click(function() {
+	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
